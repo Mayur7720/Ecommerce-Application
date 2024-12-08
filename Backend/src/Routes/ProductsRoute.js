dd6464c593b70cc1417d25fd227818aa76da4293
Mayur7720 <mayurkondhare7720@gmail.com>
2024-12-08 11:53:56 +0530

    added auth middlerware to route

--- Backend/src/Routes/ProductsRoute.js
+++ Backend/src/Routes/ProductsRoute.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const ProductsController = require("../Controller/Products/ProductsController");
+const verifyToken = require("../Middleware/auth.middleware"); // for login
 
 router.route("/").get(ProductsController.getAllProducts);
-router.route("/:id").get(ProductsController.getSingleProduct);
+router.route("/:id").get(verifyToken, ProductsController.getSingleProduct);
 module.exports = router;
