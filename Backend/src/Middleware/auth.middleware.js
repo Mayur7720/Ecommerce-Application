5bb2cb9bbc518a10222b602c1fe71e1a087703f5
Mayur7720 <mayurkondhare7720@gmail.com>
2024-12-08 11:50:59 +0530

    changing auth middleware

--- Backend/src/Middleware/auth.middleware.js
+++ Backend/src/Middleware/auth.middleware.js
@@ -3,27 +3,30 @@ const User = require("../Model/UserModel/User.model");
 
 const verifyToken = async (req, res, next) => {
   try {
-
     const token =
-      req.cookies?.accessToken ||
-      req.header("Authorization")?.replace("Bearer ", "").trim();
-
+    req.cookies?.accessToken ||
+    req.header("Authorization")?.replace("Bearer ", "").trim();
+    console.log(req.cookies)
     if (!token) {
-      return res.status(401).json({ message: "Unauthorized request" });
+      req.user = null;
+      return next();
     }
-    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     const user = await User.findById(decodedToken?._id);
+
     if (!user) {
-      return res.status(401).json({ message: "Invalid Access Token" });
+      return res.status(401).json({ message: "Invalid access token" });
     }
 
     req.user = user;
     next();
   } catch (err) {
     console.error("Token verification failed:", err.message);
-    return res.status(401).json({ message: "Invalid access token" });
+    req.user = null; // Proceed without a user for unauthenticated requests
+    next();
   }
 };
 
 module.exports = verifyToken;
+
